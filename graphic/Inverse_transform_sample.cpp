77f8dc17623a31f07494574c4de9e1b5eb4b482e
SATAN_Z <249002031@qq.com>
2018-10-01 21:15:57 +0800

    实现逆变换采样
    Signed-off-by: SATAN_Z <249002031@qq.com>

---
+++ graphic/Inverse_transform_sample.cpp
@@ -0,0 +1,107 @@
+/*
+* @Author: ZhuL
+* @Date:   2018-10-01 12:44:01
+* @Last Modified by:   ZhuL
+* @Last Modified time: 2018-10-01 21:12:39
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+#include <cstdlib> 
+#include <cstring> 
+#include <algorithm> 
+#include <vector>
+
+using namespace std;
+
+double normalPdf(double x) {
+	return 1 / sqrtf(2 * M_PI) * exp(-x * x * 0.5);
+}
+
+class Sampler
+{
+public:
+	Sampler() {}
+	~Sampler() {
+		clearCdf();
+	}
+
+	Sampler(std::function<double(double)> pdf , int count , double min , double max) {
+		createCdf(pdf , count , min , max);
+	}
+
+	void createCdf(std::function<double(double)> pdf , int count , double min , double max) {
+		this->_min = min;
+		this->_max = max;
+		clearCdf();
+		_cdf_a = new double[count + 1];
+		_cdf_a[0] = 0;
+		double dx = (max - min) / count;
+		for (int i = 1; i < count; ++i) {
+			double x = min + i * dx;
+			double dy = dx * pdf(x);
+			_cdf_a[i] = _cdf_a[i - 1] + dy;
+		}
+
+		_cdf_a[count] = 1;
+		_count = count;
+	}
+
+	double random() {
+		double r = drand48();
+		double ret;
+		double * ptr = std::lower_bound(_cdf_a , _cdf_a + _count + 1 , r);
+		int offset = ptr - _cdf_a;
+		return _min + (offset * 1.0 / _count) * (_max - _min);
+	}
+
+	void setCdf(std::function<double(double)> cdf) {
+		this->cdf = cdf;
+	}
+
+	void outputCdf() {
+		for (int i = 0; i < _count + 1; ++i) {
+			cout << _cdf_a[i] << endl;
+		}
+	}
+
+protected:
+
+	void clearCdf() {
+		if (_cdf_a != nullptr) {
+			delete [] _cdf_a;
+			_cdf_a = nullptr;
+		}
+	}
+
+	double * _cdf_a = nullptr;
+
+	std::function<double(double)> cdf;
+
+	int _count;
+
+	double _min;
+
+	double _max;
+	
+};
+
+
+int main()
+{
+	// srand((unsigned)time(0));
+	auto sampler = Sampler(normalPdf , 400 , -10 , 10);
+ 
+    std::map<int, int> hist{};
+    for(int n=0; n<10000; ++n) {
+        ++hist[sampler.random()];
+    }
+    for(auto p : hist) {
+        std::cout << std::setw(2)
+                  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
+    }
+}
